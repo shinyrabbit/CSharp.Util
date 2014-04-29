16eefada661cf6fa1c6f51ac6705ab897f0c837c
John <635253084@qq.com>
2014-04-29 10:16:09 +0800

    修改通用数据库类

--- CSharp.Util/Data/DbHelper.cs
+++ CSharp.Util/Data/DbHelper.cs
@@ -19,6 +19,16 @@ namespace CSharp.Util.Data
             this.ConnectionStr = connectionString;
             providerFactory = provider;
         }
+        public DbHelper(string connecntionString, DbProviderType providerType)
+        {
+            this.ConnectionStr = connecntionString;
+            providerFactory = ProviderFactory.GetDbProviderFactory(providerType);
+            if (providerFactory == null)
+            {
+                throw new ArgumentException("provider exception");
+            }
+
+        }
         #endregion
 
         #region ExecuteNonQuery
@@ -213,21 +223,70 @@ namespace CSharp.Util.Data
         }
 
     }
-    #region 暂时不需要
     public class ProviderFactory
     {
         private static Dictionary<DbProviderType, string> providerInvariantNames = new Dictionary<DbProviderType, string>();
         private static Dictionary<DbProviderType, DbProviderFactory> providerFactoies = new Dictionary<DbProviderType, DbProviderFactory>(20);
 
+        static ProviderFactory()
+        {
+            //加载已知的数据库访问类的程序集  
+            providerInvariantNames.Add(DbProviderType.SqlServer, "System.Data.SqlClient");
+            providerInvariantNames.Add(DbProviderType.OleDb, "System.Data.OleDb");
+            providerInvariantNames.Add(DbProviderType.ODBC, "System.Data.ODBC");
+            providerInvariantNames.Add(DbProviderType.Oracle, "Oracle.DataAccess.Client");
+            providerInvariantNames.Add(DbProviderType.MySql, "MySql.Data.MySqlClient");
+            providerInvariantNames.Add(DbProviderType.SQLite, "System.Data.SQLite");
+            providerInvariantNames.Add(DbProviderType.Firebird, "FirebirdSql.Data.Firebird");
+            providerInvariantNames.Add(DbProviderType.PostgreSql, "Npgsql");
+            providerInvariantNames.Add(DbProviderType.DB2, "IBM.Data.DB2.iSeries");
+            providerInvariantNames.Add(DbProviderType.Informix, "IBM.Data.Informix");
+            providerInvariantNames.Add(DbProviderType.SqlServerCe, "System.Data.SqlServerCe");
+        }
 
+        //public static string GetProviderInvariantName(DbProviderType providerType)
+        //{
+
+        //}
+
+        public static DbProviderFactory GetDbProviderFactory(DbProviderType providerType)
+        {
+            if (!providerFactoies.ContainsKey(providerType))
+            {
+                //TODO 加载未知provider
+                providerFactoies.Add(providerType, ImportDbProviderFactory(providerType));
+            }
+            return providerFactoies[providerType];
+        }
+
+        private static DbProviderFactory ImportDbProviderFactory(DbProviderType providerType)
+        {
+            string providerName = providerInvariantNames[providerType];
+            DbProviderFactory factory = null;
+            try
+            {
+                factory = DbProviderFactories.GetFactory(providerName);
+            }
+            catch (ArgumentException e)
+            {
+                factory = null;
+            }
+            return factory;
+        }
     }
 
     public enum DbProviderType : byte
     {
         SqlServer,
         MySql,
-        Oracle
+        Oracle,
+        ODBC,
+        OleDb,
+        SQLite,
+        Firebird,
+        PostgreSql,
+        DB2,
+        Informix,
+        SqlServerCe
     }
-    #endregion
-
 }
